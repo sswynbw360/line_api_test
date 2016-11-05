0fbd47f1e40d3621dda83fbbf1e50ac8059fce87
小野祐輔 <yusuke@MacBook-Pro.local>
2016-11-05 19:46:54 +0900

    no message

--- web/callback.php
+++ web/callback.php
@@ -51,11 +51,11 @@ if ("message" == $event->type) {            //一般的なメッセージ(文字
     // カルーセルに付与するボタンを作る
       $action = new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder("クリックしてね", "https://" . $_SERVER['SERVER_NAME'] . "/kyojin.jpeg");
     // カルーセルのカラムを作成する
+    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("$a");
+    $response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
       $column = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("タイトル(40文字以内)","http://www.hivelocity.co.jp/wp-content/uploads/2015/09/001.jpg" , [$action]);
       $columns[] = $column;
       $a++;
-      $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("$a");
-      $response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
       if($a>=4){
         brake;
       }
