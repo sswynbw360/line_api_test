c4c771ac1f76e0bafbed7ee974a014442afb31b3
小野祐輔 <yusuke@MacBook-Pro-2.local>
2016-11-18 17:39:42 +0900

    no message

--- web/callback.php
+++ web/callback.php
@@ -49,10 +49,10 @@ if ("message" == $event->type) {            //一般的なメッセージ(文字
       $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("お疲れ様！\n飽きたら退出させてね！");
       $response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
 
-      $area = new \LINE\LINEBot\MessageBuilder\ImagemapMessageBuilder\AreaBuilder(1040,1040);
-      $action = new \LINE\LINEBot\MessageBuilder\ImagemapMessageBuilder\ImagemapMessageActionBuilder("@member",$area);
-      $basesize = new \LINE\LINEBot\MessageBuilder\Imagemap\BaseSizeBuilder(1040,1040);
-      $imagemap = new \LINE\LINEBot\MessageBuilder\ImagemapMessageBuilder("https://" . $_SERVER['SERVER_NAME'] . "/","エラー","$basesize","$action");
+      $area = new \LINE\LINEBot\ImagemapMessageBuilder\AreaBuilder(0,0,1040,1040);
+      $action = new \LINE\LINEBot\ImagemapMessageBuilder\ImagemapMessageActionBuilder("@member",$area);
+      $basesize = new \LINE\LINEBot\Imagemap\BaseSizeBuilder(1040,1040);
+      $imagemap = new \LINE\LINEBot\ImagemapMessageBuilder("https://" . $_SERVER['SERVER_NAME'] . "/","エラー","$basesize","$action");
       $response = $bot->pushMessage($event->source->userId, $imagemap);
 
     } else if ("text" == $event->message->type) {
