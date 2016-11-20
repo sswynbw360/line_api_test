dbf7c512a106cea6170b7c1f3372a264bab2ac7d
小野祐輔 <yusuke@MacBook-Pro-2.local>
2016-11-20 09:57:34 +0900

    no message

--- web/callback.php
+++ web/callback.php
@@ -51,7 +51,7 @@ if ("message" == $event->type) {            //一般的なメッセージ(文字
 
       $area = new \LINE\LINEBot\ImagemapActionBuilder\AreaBuilder(0,0,1040,1040);
       $text = new \LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder("@member",$area);
-      $basesize = new \LINE\LINEBot\MessageBuilder\Imagemap\BaseSizeBuilder{1040,1040};
+      $basesize = new \LINE\LINEBot\MessageBuilder\Imagemap\BaseSizeBuilder({1040,1040});
       $action = new \LINE\LINEBot\ImagemapActionBuilder();
       $imagemap = new \LINE\LINEBot\MessageBuilder\ImagemapMessageBuilder("https://" . $_SERVER['SERVER_NAME'] . "/","エラー",$basesize,$action);
       $response = $bot->pushMessage($event->source->userId, $imagemap);
